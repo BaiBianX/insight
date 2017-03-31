8ad3e8ed35e76488d5c38793dfc707c7626ccede
draveness <i@draveness.me>
2017-03-31 19:53:56 +0800

    Fix double quote string

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/var/www/insight"
+set :deploy_to,       '/var/www/insight'
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
