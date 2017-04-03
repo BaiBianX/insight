e9f8f2158de108cf2e52d0ffed066d9b7dc21027
draveness <i@draveness.me>
2017-04-03 15:15:24 +0800

    Autofix code style by rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  enum gender: [:male, :female]
+  enum gender: %i(male female)
 
   validates :mobile, presence: true
   validates :password_digest, presence: true
