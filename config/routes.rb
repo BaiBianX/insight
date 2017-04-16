ee36d86707942085f53cc8d26ca28219b3b26f60
draveness <i@draveness.me>
2017-04-16 11:11:50 +0800

    configure database for category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :categories
   scope :api, defaults: { format: :json } do
     resources :users, except: [:destroy]
     resources :sessions, only: %i(create destroy)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409161200) do
+ActiveRecord::Schema.define(version: 20170416030935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "mobile"
